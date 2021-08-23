2365527139dbaa62dcb6c039efb1c6749b0d2096
selva prasath <selva.prasath2706@gmail.com>
2021-08-23 18:01:41 +0530

    Added a feature to update the data in sheets automatically

--- gpa_bot.py
+++ gpa_bot.py
@@ -5,7 +5,7 @@ from discord import Color
 from discord.ext import commands
 from dotenv import load_dotenv
 import json
-
+import pandas as pd
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix=['Say ', 'Say', 'say', 'say '])
@@ -22,7 +22,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     brooklyn_99_quotes = [
-        'Senkottu ve saranam etirthaal marnam',
+        'HI Welcome',
     ]
     if message.content == 'Senkottu':
         response = random.choice(brooklyn_99_quotes)
@@ -33,9 +33,15 @@ async def on_message(message):
 async def nine_nine(ctx):
     await ctx.send('Good night')
 
-@bot.command(name='akshatha_is_cute st')
-async def saygm(ctx):
-    await ctx.send('Ishtathukku Solla mudiyaathu poda')
+@bot.command(name="gpaupdate")
+async def gpaupdate(ctx):
+    sheet_id = "1ZFuspycx3a3b_ds1m4uQm73hfEkZroleCORhxBNxH9c"
+    sheet_name = "Sheet1"
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+    pf = pd.read_csv(url)
+    # print(pf)
+    pf.to_json(r'details.json')
+    await ctx.send(f"Gpa Updated")
 
 
 @bot.command(name='Gpa')
@@ -43,25 +49,23 @@ async def saygpa(ctx,regno="null"):
     status = False
     print(regno)
     try:
-        f = open('details.json','r')
-        data = json.load(f)
-        for details in data:
-          if (details['field2']==regno):
-              status = True
-              sem1_gpa = details['field4']
-              sem2_gpa = details['field5']
-              sem2_cgpa = details['field6']
-              sem3_gpa = details['field7']
-              sem3_cgpa = details['field8']
-              sem4_gpa = details['field9']
-              sem4_cgpa = details['field10']
-              sem5_gpa = details['field11']
-              sem5_cgpa = details['field12']
-              break
-          else:
-              status = False
-
-        f.close()
+        with open('details.json', 'r') as f:
+            data = json.load(f)
+            namedata = data['REG No. ']
+            for i in range(len(namedata)):
+                if (str(namedata[str(i)]) == regno):
+                    status = True
+                    print(data['NAMES '][str(i)])
+                    sem1_gpa = data['SEM I GPA'][str(i)]
+                    sem2_gpa = data['SEM II GPA'][str(i)]
+                    sem3_gpa = data['SEM III GPA'][str(i)]
+                    sem4_gpa = data['SEM IV GPA'][str(i)]
+                    sem5_gpa = data['SEM V GPA'][str(i)]
+                    cgpa = data['CGPA.3'][str(i)]
+                    break
+                else:
+                    status = False
+            f.close()
     except Exception:
         status = False
     message = discord.Embed(
@@ -73,64 +77,13 @@ async def saygpa(ctx,regno="null"):
         message.add_field(name="Reg no", value="Please Enter your Reg no")
         await ctx.send(embed=message)
     elif(status):
+        message.add_field(name="Reg no", value=regno)
         message.add_field(name="Semester 1", value=sem1_gpa)
         message.add_field(name="Semester 2", value=sem2_gpa)
-        message.add_field(name="Semester 2 Cgpa", value=sem2_cgpa)
         message.add_field(name="Semester 3", value=sem3_gpa)
-        message.add_field(name="Semester 3 Cgpa", value=sem3_cgpa)
         message.add_field(name="Semester 4", value=sem4_gpa)
-        message.add_field(name="Semester 4 Cgpa", value=sem4_cgpa)
         message.add_field(name="Semester 5", value=sem5_gpa)
-        message.add_field(name="Semester 5 Cgpa", value=sem5_cgpa)
-        await ctx.send(embed=message)
-    else:
-        message.add_field(name="Reg No", value="Register No you have entered Not found")
-        await ctx.send(embed=message)
-
-@bot.command(name='gpa')
-async def saygpa(ctx,regno="null"):
-    status = False
-    print(regno)
-    try:
-        f = open('details.json','r')
-        data = json.load(f)
-        for details in data:
-          if (details['field2']==regno):
-              status = True
-              sem1_gpa = details['field4']
-              sem2_gpa = details['field5']
-              sem2_cgpa = details['field6']
-              sem3_gpa = details['field7']
-              sem3_cgpa = details['field8']
-              sem4_gpa = details['field9']
-              sem4_cgpa = details['field10']
-              sem5_gpa = details['field11']
-              sem5_cgpa = details['field12']
-              break
-          else:
-              status = False
-
-        f.close()
-    except Exception:
-        status = False
-    message = discord.Embed(
-        title="Gpa/Cgpa Of 5 Semesters",
-        description="",
-        color=Color.teal(),
-    )
-    if (regno == "null"):
-        message.add_field(name="Reg no", value="Please Enter your Reg no")
-        await ctx.send(embed=message)
-    elif(status):
-        message.add_field(name="Semester 1", value=sem1_gpa)
-        message.add_field(name="Semester 2", value=sem2_gpa)
-        message.add_field(name="Semester 2 Cgpa", value=sem2_cgpa)
-        message.add_field(name="Semester 3", value=sem3_gpa)
-        message.add_field(name="Semester 3 Cgpa", value=sem3_cgpa)
-        message.add_field(name="Semester 4", value=sem4_gpa)
-        message.add_field(name="Semester 4 Cgpa", value=sem4_cgpa)
-        message.add_field(name="Semester 5", value=sem5_gpa)
-        message.add_field(name="Semester 5 Cgpa", value=sem5_cgpa)
+        message.add_field(name="Cgpa", value=cgpa)
         await ctx.send(embed=message)
     else:
         message.add_field(name="Reg No", value="Register No you have entered Not found")
